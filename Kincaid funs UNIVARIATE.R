467756d40f86752392fc1ab86a8d447c6dcc593b
Bio240 <34696682+Bio240@users.noreply.github.com>
2018-06-20 00:36:02 -0400

    Update Kincaid funs UNIVARIATE.R

--- Kincaid funs UNIVARIATE.R
+++ Kincaid funs UNIVARIATE.R
@@ -1,7 +1,7 @@
 
 # Kincaid funs UNIVARIATE.R     20 December 2017     Author: Dwight Kincaid
 #                                                    dwight.kincaid@lehman.cuny.edu
-##
+##    
 ##
 ##    FUNCTION  DEFINITIONS  --  for use on a single, numeric sample, e.g., a vector
 ##
